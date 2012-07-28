683f5ba26af43a11aba07f323ecf4c695c403fb4
Dirk Eisenberg <dirk.eisenberg@gmail.com>
2012-07-28 15:10:49 +0200

    added the CSS fixed as long as the core team needs

--- lib/premailer-rails3/hook.rb
+++ lib/premailer-rails3/hook.rb
@@ -5,38 +5,56 @@ module PremailerRails
       # case, if the mail content type is text/html, the body part will be the
       # html body.
       if message.html_part
-        html_body       = message.html_part.body.to_s
-        needs_multipart = true
-        message.parts.delete(message.html_part)
+        html_body = message.html_part.body.to_s
       elsif message.content_type =~ /text\/html/
-        html_body       = message.body.to_s
-        message.body    = nil
-        needs_multipart = PremailerRails.config[:generate_text_part]
+        html_body = message.body.to_s
+        message.body = nil
       end
 
       if html_body
         premailer = Premailer.new(html_body)
         charset   = message.charset
 
-        if needs_multipart
-          # IMPORTANT: Plain text part must be generated before CSS is inlined.
-          # Not doing so results in CSS declarations visible in the plain text
-          # part.
-          if PremailerRails.config[:generate_text_part] \
-          and not message.text_part
-            message.text_part do
-              content_type "text/plain; charset=#{charset}"
-              body premailer.to_plain_text
-            end
+
+        # IMPORTANT: Plain text part must be generated before CSS is inlined.
+        # Not doing so results in CSS declarations visible in the plain text
+        # part.
+
+        unless message.text_part
+          text_part = {
+            :content_type => "text/plain; charset=#{charset}",
+            :body => premailer.to_plain_text
+          }
+        end
+
+        html_part = {
+          :content_type => "text/html; charset=#{charset}",
+          :body => premailer.to_inline_css
+        }
+
+        if message.text_part && message.html_part
+          message.html_part.body = html_part[:body]
+          message.html_part.content_type = html_part[:content_type]
+
+        elsif message.html_part
+          message.parts.delete_if { |p| p.mime_type == 'text/html' && !p.attachment? }
+
+          message.part :content_type => "multipart/alternative", :content_disposition => "inline" do |p|
+            p.part text_part
+            p.part html_part
+          end
+        else
+          message.text_part do
+            body text_part[:body]
+            content_type text_part[:content_type]
           end
 
           message.html_part do
-            content_type "text/html; charset=#{charset}"
-            body premailer.to_inline_css
+            body html_part[:body]
+            content_type html_part[:content_type]
           end
-        else
-          message.body = premailer.to_inline_css
         end
+
       end
     end
   end
--- lib/premailer-rails3/version.rb
+++ lib/premailer-rails3/version.rb
@@ -1,3 +1,3 @@
 module PremailerRails
-  VERSION = '1.3.1'
+  VERSION = "1.1.0"
 end
